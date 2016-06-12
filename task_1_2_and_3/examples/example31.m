cd7a1e6a1bf4f1993fe19f13e46d2d50895b3b2b
neocampos <nelsoncscampos@gmail.com>
2016-06-12 18:45:08 -0300

    Update example31.m

--- task_1_2_and_3/examples/example31.m
+++ task_1_2_and_3/examples/example31.m
@@ -2,22 +2,33 @@
 % Systems Identification 
 % Example 3.1 of the book System Identification: Theory for the User (2nd Edition).
 
-t = 1:5;
-e = [1 2 3 4 5]; %randn(1, size(t,2))
-a = 0.5;
+clc
+close all
+clear all
 
-e2 = zeros(1, size(t,2)); %e(t-k)
+t = 0:1:4;
+
+e = [1 2 3 4 5];%randn(1, size(t,2)); %definition of e(t)
+c = 2;
+v = e;
+
+%definition of v(t)
+for k=2:size(t,2)
+    v(k) = e(k)+c*e(k-1);
+end
+
+e2 = zeros(1, size(t,2)); %e2 is e(t) retrieved from v(t)
+%This part makes the convolution
 tau = 0:size(t,2);
-v = zeros(1,size(t,2)); %v(t)
-for index_tau = 1: 5;
+for index_tau = 1: size(t,2);
     for k=1:size(t,2)
-        if(k-tau(index_tau) >=1&& k-tau(index_tau) <= size(t,2))
-            e2(k) = e(k-tau(index_tau));
+        if(k-tau(index_tau) >=1 && k-tau(index_tau) <= size(t,2))
+            v2(k) = v(k-tau(index_tau));
         else
-            e2(k) = 0;    
+            v2(k) = 0;    
         end
     end
-    v = v + a^tau(index_tau)*e2; %e(t) is retrieved here
+    e2 = e2 + (-c)^tau(index_tau)*v2; %e(t) is retrieved here
 end
 
 %Then the one step ahead predictor is v(t|t-1) = v(t)-e(t)
