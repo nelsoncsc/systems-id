e581f1dc6346fa1bb6ed4835f1b2d117be66378e
neocampos <nelsoncscampos@gmail.com>
2016-06-12 18:11:07 -0300

    Create example21.m
    Example 2.1 of the book System Identification: Theory for the User (2nd Edition)

---
+++ task_1_2_and_3/examples/example21.m
@@ -0,0 +1,29 @@
+%Example 2.1
+clc
+close all
+clear all
+
+N0 = 5;
+s = 20;
+N = s*N0;
+A = 2;
+t = [1:1:N];
+w0 = 2*pi/N0;
+
+u = A*cos(w0*t);
+
+figure(1)
+hold on
+plot(t,u), xlabel('Tempo (t)'),ylabel('u(t) = A*cos(w0*t)'), title('Sinal de entrada')
+
+t=1:N;
+w=linspace(0,pi,129);
+for index_w=1:size(w,2)
+    Un(index_w)=sum(u.*exp(-i*w(index_w)*t));
+end
+Un=1/sqrt(N)*Un;
+
+figure(2)
+plot(w,abs(Un).^2,'r'),xlabel('Frequência (w)'),ylabel('|U(w)|^2'), title('DEP de u(t)')
+
+hold off
